2a9d93349889b6d1f9eca7ae9bf226d2f90128ae
lilkaz <39888536+lilkaz@users.noreply.github.com>
2025-02-24 21:50:29 -0500

    spawning traps

--- main.gd
+++ main.gd
@@ -1,11 +1,22 @@
 extends Node
 
+@onready var bear_trap_spawn: Area2D = $BearTrapSpawn
+@onready var bear_trap_spawn_2: Area2D = $BearTrapSpawn2
+@onready var bear_trap_spawn_3: Area2D = $BearTrapSpawn3
+@onready var bear_trap_spawn_4: Area2D = $BearTrapSpawn4
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	if bear_trap_spawn == null:
+		print("spawnArea node not found")
+		
+	for _i in range(2):
+		bear_trap_spawn.spawn_bear_trap()
+		bear_trap_spawn_2.spawn_bear_trap()
+		bear_trap_spawn_3.spawn_bear_trap()
+		bear_trap_spawn_4.spawn_bear_trap()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
